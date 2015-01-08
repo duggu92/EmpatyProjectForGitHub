23db2983c1777e2c2bc1fdeabaec38d90733e3ad
DazzledApps <ashish.verma@dazzledapps.com>
2015-01-09 03:18:57 +0530

    V0.2 DazzledApps

---
+++ Assets/Script/MainMenuScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		CheckHello ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void CheckHello()
+	{
+
+	}
+}
